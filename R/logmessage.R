72af0e96ce074a6256db911aef038ac3b799b656
Jeroen Claes <jeroenclaes@gmail.com>
2017-10-02 09:22:59 +0200

    Update logmessage.R

--- R/logmessage.R
+++ R/logmessage.R
@@ -1,15 +1,14 @@
-#' Simple function to generate logs with timestamps
+#' Simple function to write logs with timestamps to stderr
 #'
 #' @param string The message
 #' @param verbose Verbosity: if TRUE, the function will print the message, if FALSE the function will do nothing at all
 #'
-#' @return None. The message with an added timestamp will be printed to the screen
+#' @return None. The message with an added timestamp will be written to stderr
 #' @export
 #'
 #' @examples
 logmessage <- function(string, verbose=T) {
-  if(isTRUE(verbose)) {
-
-  print(paste(Sys.time(),  string, sep=": "))
+if(isTRUE(verbose)) {
+  write(paste(Sys.time(),  string, sep=": "), file=stderr())
   }
 }
